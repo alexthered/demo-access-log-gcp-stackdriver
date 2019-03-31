40a02a8847b6b7bfb13848812c4446dbf5582bf2
Hiep Doan <hiep.dv179@gmail.com>
2019-03-31 21:46:39 +0200

    🎉 Setup the project

---
+++ src/main/java/me/alexthered/demoaccessloggcpstackdriver/DemoAccessLogGcpStackdriverApplication.java
@@ -0,0 +1,13 @@
+package me.alexthered.demoaccessloggcpstackdriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAccessLogGcpStackdriverApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoAccessLogGcpStackdriverApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/alexthered/demoaccessloggcpstackdriver/DemoAccessLogGcpStackdriverApplicationTests.java
@@ -0,0 +1,16 @@
+package me.alexthered.demoaccessloggcpstackdriver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoAccessLogGcpStackdriverApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
